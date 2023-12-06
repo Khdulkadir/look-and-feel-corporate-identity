1d932e2edd47bda4824c13029e6b1be760dd0b21
Khdulkadir <khdulkadir@gmail.com>
2023-12-06 18:59:15 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,3 @@
-// SIDEBAR GIVE ACTIVE ITEM BACKGROUND COLOR
-
-// const nav = document.getElementsByClassName("menu-links")[0];
-// const listItems = nav.getElementsByTagName("li");
-
-// function setActive(index) {
-//   for (let i = 0; i < listItems.length; i++) {
-//     listItems[i].classList.remove("active");
-//   }
-
-//   listItems[index].classList.add("active");
-// }
-
-// setActive(1);
-
 // MOBILE MENU COLLAPSE
 
 const sidebar = document.querySelector(".sidebar");
@@ -21,4 +6,41 @@ const hamburgermenu = document.querySelector(".bx.bx-menu.icon");
 hamburgermenu.addEventListener("click", () => {
   sidebar.classList.toggle("collapsed");
   hamburgermenu.classList.toggle("collapsed");
+});
+
+// BOOK SLIDER LEESLIJST
+
+document.addEventListener("DOMContentLoaded", function () {
+  const bookList = document.querySelector(".book-list");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  const bookWidth = document.querySelector(".book").offsetWidth + 60; // Adjust margin-left, margin-right, and sidebar width
+  const booksToShow = 3;
+  const totalBooks = document.querySelectorAll(".book").length;
+  let currentPosition = 0;
+  const maxScroll = Math.ceil(totalBooks / booksToShow - 1) * bookWidth;
+
+  nextButton.addEventListener("click", function () {
+    const scrollAmount = bookWidth;
+    currentPosition += scrollAmount;
+    if (currentPosition > maxScroll) {
+      currentPosition = maxScroll;
+    }
+    bookList.scrollTo({
+      left: currentPosition,
+      behavior: "smooth",
+    });
+  });
+
+  prevButton.addEventListener("click", function () {
+    const scrollAmount = bookWidth;
+    currentPosition -= scrollAmount;
+    if (currentPosition < 0) {
+      currentPosition = 0;
+    }
+    bookList.scrollTo({
+      left: currentPosition,
+      behavior: "smooth",
+    });
+  });
 });
