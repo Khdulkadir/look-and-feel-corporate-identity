7ee7a8a9bcba5805e9e202ecdc33902176fd546b
Khdulkadir <khdulkadir@gmail.com>
2023-12-07 19:19:18 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -10,37 +10,19 @@ hamburgermenu.addEventListener("click", () => {
 
 // BOOK SLIDER LEESLIJST
 
-document.addEventListener("DOMContentLoaded", function () {
-  const bookList = document.querySelector(".book-list");
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
-  const bookWidth = document.querySelector(".book").offsetWidth + 60; // Adjust margin-left, margin-right, and sidebar width
-  const booksToShow = 3;
-  const totalBooks = document.querySelectorAll(".book").length;
-  let currentPosition = 0;
-  const maxScroll = Math.ceil(totalBooks / booksToShow - 1) * bookWidth;
+const bookList = document.querySelector(".book-list");
+const distance = 220;
 
-  nextButton.addEventListener("click", function () {
-    const scrollAmount = bookWidth;
-    currentPosition += scrollAmount;
-    if (currentPosition > maxScroll) {
-      currentPosition = maxScroll;
-    }
-    bookList.scrollTo({
-      left: currentPosition,
-      behavior: "smooth",
-    });
+function scrollRight() {
+  bookList.scrollBy({
+    left: distance,
+    behavior: "smooth",
   });
+}
 
-  prevButton.addEventListener("click", function () {
-    const scrollAmount = bookWidth;
-    currentPosition -= scrollAmount;
-    if (currentPosition < 0) {
-      currentPosition = 0;
-    }
-    bookList.scrollTo({
-      left: currentPosition,
-      behavior: "smooth",
-    });
+function scrollLft() {
+  bookList.scrollBy({
+    left: -distance,
+    behavior: "smooth",
   });
-});
+}
