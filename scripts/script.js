1050dff9e03d06bd2a42280f059881d04523b5b4
Khdulkadir <khdulkadir@gmail.com>
2023-12-20 23:29:13 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,42 @@ function scrollLft() {
     behavior: "smooth",
   });
 }
+
+// BOOK SLIDER POP UP
+
+// Get all book elements
+const bookElements = document.querySelectorAll(".book");
+
+// Get the popup element and its components
+const popup = document.querySelector(".popup");
+const popupTitle = document.querySelector(".popup-title");
+const popupInfo = document.querySelector(".popup-info");
+const popupImg = document.querySelector(".popup-book-img");
+
+// Function to show the popup
+function showPopup(title, info, imgSrc) {
+  popupTitle.textContent = title;
+  popupInfo.textContent = info;
+  popupImg.src = imgSrc;
+  popup.style.display = "flex";
+}
+
+// Function to hide the popup
+function hidePopup() {
+  popup.style.display = "none";
+}
+
+// Add click event to each book element
+bookElements.forEach((book, index) => {
+  book.addEventListener("click", () => {
+    const title = book.querySelector(".book-title").textContent;
+    const writer = book.querySelector(".writer").textContent;
+    const info = book.querySelector(".info-text").textContent;
+    const imgSrc = book.querySelector("img").src;
+    showPopup(title, info, imgSrc);
+  });
+});
+
+// Add click event to close button in the popup
+const closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", hidePopup);
